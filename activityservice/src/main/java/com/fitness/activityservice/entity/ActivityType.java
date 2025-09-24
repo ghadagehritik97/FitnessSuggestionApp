adc81464af9373d898b5f21718142fa0b1055850
Hritik Ghadage <ghadagehritik97@gmail.com>
2025-09-25 00:09:25 +0530

    added basic setup for activity service

---
+++ activityservice/src/main/java/com/fitness/activityservice/entity/ActivityType.java
@@ -0,0 +1,14 @@
+package com.fitness.activityservice.entity;
+
+public enum ActivityType {
+    RUNNING,
+    CYCLING,
+    SWIMMING,
+    WALKING,
+    YOGA,
+    STRENGTH_TRAINING,
+    HIIT,
+    DANCE,
+    PILATES,
+    OTHER;
+}
