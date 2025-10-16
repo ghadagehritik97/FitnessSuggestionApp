f82ab315d1cf59607b9abc2ba2e9e50db2f6d79e
Hritik Ghadage <ghadagehritik97@gmail.com>
2025-10-17 01:15:52 +0530

    added config server changes and gateway routing

--- aiservice/src/main/java/com/fitness/aiservice/controller/RecommendationController.java
+++ aiservice/src/main/java/com/fitness/aiservice/controller/RecommendationController.java
@@ -5,22 +5,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import com.fitness.aiservice.entity.Recommendations;
 
 @RestController
+@RequestMapping("/recommendations")
 public class RecommendationController {
     @Autowired
     private RecommendationService recommendationService;
 
-    @GetMapping("/recommendations/user/{userId}")
+    @GetMapping("/user/{userId}")
     private ResponseEntity<List<Recommendations>> getRecommendations(@PathVariable String userId)
     {
         return ResponseEntity.ok(recommendationService.getRecommendations(userId));
     }
 
-    @GetMapping("/recommendations/activity/{activityId}")
+    @GetMapping("/activity/{activityId}")
     private ResponseEntity<Recommendations> getRecommendationsForActivity(@PathVariable String activityId)
     {
         return ResponseEntity.ok(recommendationService.getRecommendationsByActivity(activityId));
